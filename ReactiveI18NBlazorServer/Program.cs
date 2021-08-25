12985703a63fc203c70b61df4449775ac07b746a
Red Wei <hevilhuy2@gmail.com>
2021-08-25 18:14:01 +0700

    Clean template.

--- ReactiveI18NBlazorServer/Program.cs
+++ ReactiveI18NBlazorServer/Program.cs
@@ -1,13 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace ReactiveI18NBlazorServer
+namespace ReactiveI18NCookiesBlazorServer
 {
     public class Program
     {
@@ -16,11 +10,12 @@ namespace ReactiveI18NBlazorServer
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                    });
+        }
     }
 }
